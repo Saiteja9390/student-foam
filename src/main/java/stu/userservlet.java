9296333ebb977124adbc5fe33e7006290d60a23d
saiteja <vellanii.sai@gmail.com>
2026-02-08 14:33:24 +0530

    Fix servlet-class name

--- src/main/java/stu/userservlet.java
+++ src/main/java/stu/userservlet.java
@@ -11,7 +11,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
-public class userservlet extends HttpServlet {
+public class Userservlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
